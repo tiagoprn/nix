afce94b98f69c647e69e86b2a1e9899162c7da34
tiago.paranhos <tiagoprn@gmail.com>
2023-08-26 21:05:40 -0300

    feat: updated from last configuration.

--- home/scripts/hyprland-portals.sh
+++ home/scripts/hyprland-portals.sh
@@ -2,6 +2,5 @@
 
 systemctl --user stop xdg-desktop-portal-gtk.service \
     && systemctl --user stop xdg-desktop-portal-hyprland.service \
-    && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP \
     && systemctl --user start xdg-desktop-portal-gtk.service \
     && systemctl --user start xdg-desktop-portal-hyprland.service
