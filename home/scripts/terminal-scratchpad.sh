ce31a6dea15a2445cb6b0fac46315df2509fbe43
tiago.paranhos <tiagoprn@gmail.com>
2023-09-02 11:31:05 -0300

    feat: synced scripts.

--- home/scripts/terminal-scratchpad.sh
+++ home/scripts/terminal-scratchpad.sh
@@ -5,4 +5,6 @@ TMUXP_SESSIONS_FILES="/storage/src/nix/home/tmuxp/fleeting-notes.yml /storage/sr
 # alacritty --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c "tmuxp load '$TMUXP_SESSIONS_FILES'" --hold &
 # urxvt --hold --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c 'tmuxp load /storage/src/devops/tmuxp/notes-and-reminders.yml' &
 
-kitty --class scratchpad_notes_tasks_n_reminders --hold /run/current-system/sw/bin/bash -c "tmuxp load $TMUXP_SESSIONS_FILES" &
+# KITTY_DISABLE_WAYLAND=1 kitty --class scratchpad_notes_tasks_n_reminders --hold /run/current-system/sw/bin/bash -c "tmuxp load $TMUXP_SESSIONS_FILES" &
+
+KITTY_DISABLE_WAYLAND=1 kitty --hold /run/current-system/sw/bin/bash -c "echo 'You can run e.g. tmuxp-notes_tasks_n_reminders.sh'"
